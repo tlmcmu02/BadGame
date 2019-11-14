f207cfa575e1820e7d929907b5d050c41ee98a1a
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-11-14 09:22:37 -0500

    First Commit (CLEAN SLATE)

---
+++ modules/secondlevel.lua
@@ -0,0 +1,17 @@
+love.graphics.setDefaultFilter('nearest', 'nearest')
+local Map = require 'core/map'
+local gamera = require 'core/gamera'
+local Util = require 'core/util'
+local anim8 = require 'core/anim8'
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
