37577678052927210c84f7fff686d196c8f7a9bd
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-11-22 09:50:17 -0500

    beep

--- modules/firstlevel.lua
+++ modules/firstlevel.lua
@@ -4,10 +4,11 @@ local gamera = require 'core/gamera'
 local Util = require 'core/util'
 local anim8 = require 'core/anim8'
 
-function love.load()  --33, 35
-    x = 64
-    y = 64
+function love.load()
 
+    cam = gamera.new(48, 52, 8000, 6000)
+    
+    
     spritesheet = love.graphics.newImage('assets-1/Player/placehold.png')
     grid = anim8.newGrid(66, 68, 600, 504)
     walkdown = anim8.newAnimation(grid('1-7', 1), 0.09)
@@ -16,8 +17,68 @@ function love.load()  --33, 35
     idlex = anim8.newAnimation(grid('1-1', 3), 0.09)
     idleup = anim8.newAnimation(grid('1-1', 2), 0.09)
     idledown = anim8.newAnimation(grid('1-1', 1), 0.09)
-
     anim = walkdown
+    x = 128
+    y = 128
+
+    wall = love.graphics.newImage('assets-1/Wall/catacombs_0.png')
+
+    walls = {
+        {wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, 'nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil','nil', wall,},
+        {wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,wall, wall, wall,},
+    }
+    walls = Map:new(walls)
 end
 
 function love.update(dt)
@@ -28,6 +89,9 @@ function love.update(dt)
         FaceR = false
         walkup:update(dt)
         anim = walkup
+        if walls:cc(x + 16 , y + 10, 32, 32) == false  then
+            y = y - 6
+        end
     end
     if love.keyboard.isDown('down') then
         FaceU = false
@@ -36,6 +100,9 @@ function love.update(dt)
         FaceR = false
         walkdown:update(dt)
         anim = walkdown
+        if walls:cc(x + 16 , y + 22, 32, 32) == false then
+            y = y + 6
+        end
     end
     if love.keyboard.isDown('left') then
         FaceU = false
@@ -44,6 +111,9 @@ function love.update(dt)
         FaceR = false
         walkx:update(dt)
         anim = walkx
+        if walls:cc(x + 10, y + 16, 32, 32) == false  then
+            x = x - 6
+        end
     end
     if love.keyboard.isDown('right') then
         FaceU = false
@@ -52,6 +122,9 @@ function love.update(dt)
         FaceR = true
         walkx:update(dt)
         anim = walkx
+        if walls:cc(x + 22, y + 16, 32, 32) == false  then
+            x = x + 6
+        end
     end
     if love.keyboard.isDown('up') == false then
         if love.keyboard.isDown('down') == false then
@@ -73,9 +146,12 @@ function love.update(dt)
             end
         end
     end
+    cam:setPosition(x, y)
 end
 
 function love.draw()
+    cam:draw(function(camx, camy)
+    walls:draw()
     if anim == walkx then
         if FaceR == true then
             walkx:draw(spritesheet, x, y)
@@ -104,4 +180,5 @@ function love.draw()
             idlex:draw(spritesheet, x, y, rotation, -1, 1, 64, 0)
         end
     end
+end)
 end
