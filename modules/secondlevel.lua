2f3fb2c82eb59aa7d95bc7bbfe9df7c956b9b71b
tlmcmu02 <55092286+tlmcmu02@users.noreply.github.com>
2020-01-07 20:52:21 -0500

    vfxt

--- modules/secondlevel.lua
+++ modules/secondlevel.lua
@@ -13,9 +13,7 @@ local bullets = {}
 table.insert(bullets, Bullet:new(140, 140, 'S'))
 function love.load()
   
-PlayerbreakoutLoad() -- make this a class
- 
-
+Player1 = Player:new(128, 368, 'Breakout')
 ball1 = ball:new(64, 500)
 
 hpBar = {
@@ -57,9 +55,9 @@ function love.update(dt)
   ball1:update(dt)
   for i = 1, #bullets do
     bullets[i]:update(dt)
-    if cc(x + 16, y + 16, 32, 32, bullets[i].x, bullets[i].y, 16, 16) then
+    if cc(Player1.x + 16, Player1.y + 16, 32, 32, bullets[i].x, bullets[i].y, 16, 16) then
       bullets[i]:setDelete()-- then delete the bullet
-      IFrames() -- decrease player hp
+      Player1:IFrames() -- decrease player hp
     end
   end
 
@@ -67,16 +65,16 @@ function love.update(dt)
   -- if the bullet collides with the player,
   
 
-  if timerIFrames > 0 then
-    timerIFrames = timerIFrames - 1
-    Iframes = 2
+  if Player1.timerIFrames > 0 then
+    Player1.timerIFrames = Player1.timerIFrames - 1
+    Player1.Iframes = 2
 end
-if timerIFrames < 1 then
-    timerIFrames = 0
-    Iframes = 0
+if Player1.timerIFrames < 1 then
+  Player1.timerIFrames = 0
+  Player1.Iframes = 0
 end
 
-  enginebreakoutupdate(dt)
+  Player1:breakoutupdate(dt)
   if lifelost == 1 then
     lives = lives - 1
     x = 128
@@ -89,7 +87,7 @@ end
 
 
 
-  cam:setPosition(x, y)
+  cam:setPosition(Player1.x, Player1.y)
 
 end
 
@@ -99,14 +97,18 @@ function love.draw()
         mapc:draw()
         ball1:draw()
        
-        DrawbreakoutPlayer()
+        Player1:Draw()
         for i = 1, #bullets do
           bullets[i]:draw()
         end
-       
+        for i = 1, 5 do
+          for t = 1, 10 do
+          block[i][t]:draw()
+          end
+      end 
         
 
-        love.graphics.print(Iframes, x, y)
+        love.graphics.print(Player1.Iframes, x, y)
 
         
         
@@ -127,6 +129,411 @@ end
 function endofdash()
   dashattack = 0
 end
+
+function updashblockcc()
+    if block[1][1].updashcollision == 0 then
+    if block[1][2].updashcollision == 0 then
+    if block[1][3].updashcollision == 0 then
+    if block[1][4].updashcollision == 0 then
+    if block[1][5].updashcollision == 0 then
+    if block[1][6].updashcollision == 0 then
+    if block[1][7].updashcollision == 0 then
+    if block[1][8].updashcollision == 0 then
+    if block[1][9].updashcollision == 0 then
+    if block[1][10].updashcollision == 0 then
+    if block[2][1].updashcollision == 0 then
+    if block[2][2].updashcollision == 0 then
+    if block[2][3].updashcollision == 0 then
+    if block[2][4].updashcollision == 0 then
+    if block[2][5].updashcollision == 0 then
+    if block[2][6].updashcollision == 0 then
+    if block[2][7].updashcollision == 0 then
+    if block[2][8].updashcollision == 0 then
+    if block[2][9].updashcollision == 0 then
+    if block[2][10].updashcollision == 0 then
+    if block[3][1].updashcollision == 0 then
+    if block[3][2].updashcollision == 0 then
+    if block[3][3].updashcollision == 0 then
+    if block[3][4].updashcollision == 0 then
+    if block[3][5].updashcollision == 0 then
+    if block[3][6].updashcollision == 0 then
+    if block[3][7].updashcollision == 0 then
+    if block[3][8].updashcollision == 0 then
+    if block[3][9].updashcollision == 0 then
+    if block[3][10].updashcollision == 0 then
+    if block[4][1].updashcollision == 0 then
+    if block[4][2].updashcollision == 0 then
+    if block[4][3].updashcollision == 0 then
+    if block[4][4].updashcollision == 0 then
+    if block[4][5].updashcollision == 0 then
+    if block[4][6].updashcollision == 0 then
+    if block[4][7].updashcollision == 0 then
+    if block[4][8].updashcollision == 0 then
+    if block[4][9].updashcollision == 0 then
+    if block[4][10].updashcollision == 0 then
+    if block[5][1].updashcollision == 0 then
+    if block[5][2].updashcollision == 0 then
+    if block[5][3].updashcollision == 0 then
+    if block[5][4].updashcollision == 0 then
+    if block[5][5].updashcollision == 0 then
+    if block[5][6].updashcollision == 0 then
+    if block[5][7].updashcollision == 0 then
+    if block[5][8].updashcollision == 0 then
+    if block[5][9].updashcollision == 0 then
+    if block[5][10].updashcollision == 0 then
+      return true  
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end 
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end 
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end 
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end 
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end
+    else  
+    return false
+    end      
+  end 
+end    
+
+
+function upblockcc()
+  if block[1][1].upcollision == 0 then
+  if block[1][2].upcollision == 0 then
+  if block[1][3].upcollision == 0 then
+  if block[1][4].upcollision == 0 then
+  if block[1][5].upcollision == 0 then
+  if block[1][6].upcollision == 0 then
+  if block[1][7].upcollision == 0 then
+  if block[1][8].upcollision == 0 then
+  if block[1][9].upcollision == 0 then
+  if block[1][10].upcollision == 0 then
+  if block[2][1].upcollision == 0 then
+  if block[2][2].upcollision == 0 then
+  if block[2][3].upcollision == 0 then
+  if block[2][4].upcollision == 0 then
+  if block[2][5].upcollision == 0 then
+  if block[2][6].upcollision == 0 then
+  if block[2][7].upcollision == 0 then
+  if block[2][8].upcollision == 0 then
+  if block[2][9].upcollision == 0 then
+  if block[2][10].upcollision == 0 then
+  if block[3][1].upcollision == 0 then
+  if block[3][2].upcollision == 0 then
+  if block[3][3].upcollision == 0 then
+  if block[3][4].upcollision == 0 then
+  if block[3][5].upcollision == 0 then
+  if block[3][6].upcollision == 0 then
+  if block[3][7].upcollision == 0 then
+  if block[3][8].upcollision == 0 then
+  if block[3][9].upcollision == 0 then
+  if block[3][10].upcollision == 0 then
+  if block[4][1].upcollision == 0 then
+  if block[4][2].upcollision == 0 then
+  if block[4][3].upcollision == 0 then
+  if block[4][4].upcollision == 0 then
+  if block[4][5].upcollision == 0 then
+  if block[4][6].upcollision == 0 then
+  if block[4][7].upcollision == 0 then
+  if block[4][8].upcollision == 0 then
+  if block[4][9].upcollision == 0 then
+  if block[4][10].upcollision == 0 then
+  if block[5][1].upcollision == 0 then
+  if block[5][2].upcollision == 0 then
+  if block[5][3].upcollision == 0 then
+  if block[5][4].upcollision == 0 then
+  if block[5][5].upcollision == 0 then
+  if block[5][6].upcollision == 0 then
+  if block[5][7].upcollision == 0 then
+  if block[5][8].upcollision == 0 then
+  if block[5][9].upcollision == 0 then
+  if block[5][10].upcollision == 0 then
+    return true  
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end 
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end 
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end 
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end 
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end
+  else  
+  return false
+  end      
+end 
+end    
 
 function love.keypressed(key)
   if key == 'space' then
